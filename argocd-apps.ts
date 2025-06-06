730df16b53ad26a724bf0016ecba5d0ee036ba59
Niklas Rosenstein <rosensteinniklas@gmail.com>
2025-06-06 12:10:40 +0200

    templatize generation of Argo Application

--- argocd-apps.ts
+++ argocd-apps.ts
@@ -2,8 +2,58 @@
  * Defines ArgoCD projects, repositories and applications.
  */
 
-import { Repository, Application } from "jsr:@gin/argocd-v1alpha1/crds";
+import { Application, Repository } from "jsr:@gin/argocd-v1alpha1/crds";
 import { Gin, SecretValue } from "jsr:@gin/core";
+
+interface GinApplication {
+  name: string;
+  deno?: {
+    allowWrite?: string[];
+    allowRead?: string[];
+    allowRun?: string[];
+  };
+}
+
+function createApplication(app: GinApplication): Application {
+  return {
+    apiVersion: "argoproj.io/v1alpha1",
+    kind: "Application",
+    metadata: {
+      name: app.name,
+      namespace: "argocd",
+    },
+    spec: {
+      project: "default",
+      destination: {
+        server: "https://kubernetes.default.svc",
+      },
+      source: {
+        repoURL: "ssh://git@github.com/NiklasRosenstein/argocd-testing.git",
+        targetRevision: "HEAD",
+        path: ".",
+        plugin: {
+          name: "gin-v1",
+          parameters: [
+            { name: "script", string: "main.ts" },
+            { name: "args", array: [app.name] },
+            {
+              name: "deno_allow_write",
+              array: ["/tmp/gin", ...app.deno?.allowWrite ?? []],
+            },
+            {
+              name: "deno_allow_read",
+              array: ["/tmp/gin", ...app.deno?.allowRead ?? []],
+            },
+            {
+              name: "deno_allow_run",
+              array: ["helm", ...app.deno?.allowRun ?? []],
+            },
+          ],
+        },
+      },
+    },
+  };
+}
 
 export default (gin: Gin) => {
   gin.emit<Repository>({
@@ -27,63 +77,6 @@ export default (gin: Gin) => {
     },
   });
 
-  gin.emit<Application>({
-    apiVersion: "argoproj.io/v1alpha1",
-    kind: "Application",
-    metadata: {
-      name: "argocd",
-      namespace: "argocd",
-    },
-    spec: {
-      project: "default",
-      destination: {
-        server: "https://kubernetes.default.svc",
-      },
-      source: {
-        repoURL: "ssh://git@github.com/NiklasRosenstein/argocd-testing.git",
-        targetRevision: "HEAD",
-        path: ".",
-        plugin: {
-          name: "gin-v1",
-          parameters: [
-            { name: "script", string: "main.ts" },
-            { name: "args", array: ["argocd"] },
-            { name: "deno_allow_write", array: ["/tmp/gin"] },
-            { name: "deno_allow_read", array: ["/tmp/gin"] },
-            { name: "deno_allow_run", array: ["helm"] },
-          ],
-        },
-      },
-    },
-  });
-
-  gin.emit<Application>({
-    apiVersion: "argoproj.io/v1alpha1",
-    kind: "Application",
-    metadata: {
-      name: "argocd-apps",
-      namespace: "argocd",
-    },
-    spec: {
-      project: "default",
-      destination: {
-        server: "https://kubernetes.default.svc",
-      },
-      source: {
-        repoURL: "ssh://git@github.com/NiklasRosenstein/argocd-testing.git",
-        targetRevision: "HEAD",
-        path: ".",
-        plugin: {
-          name: "gin-v1",
-          parameters: [
-            { name: "script", string: "main.ts" },
-            { name: "args", array: ["argocd-apps"] },
-            { name: "deno_allow_write", array: ["/tmp/gin"] },
-            { name: "deno_allow_read", array: ["/tmp/gin", "./deploy"] },
-            { name: "deno_allow_run", array: ["helm"] },
-          ],
-        },
-      },
-    },
-  });
+  gin.emit<Application>(createApplication({ name: "argocd" }));
+  gin.emit<Application>(createApplication({ name: "argocd-apps" }));
 };
